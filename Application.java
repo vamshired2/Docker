c054bad5533755d78a519163d3bd60d156869b5d
Vamshi <vamsi@gmail.com>
2025-03-06 15:16:58 +0530

    updated version

--- Application.java
+++ Application.java
@@ -1,5 +1,12 @@
-public class Application {
+public class Applicayion {
     public static void main(String[] args) {
         System.out.println("Hello, Docker!");
+        while (true) {  // Keeps the application running
+            try {
+                Thread.sleep(10000); // Sleep for 10 seconds
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
